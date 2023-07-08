95766a9ac5bbd075c44affad3bd4e9725509fceb
nicompiledev <nsrivera80@misena.edu.co>
2023-07-08 17:06:19 -0500

    methods and parameterized methods

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Llamada al método sayHello() sin argumentos
+        sayHello();
+
+        // Llamada al método sayHello() con un argumento o parametro
+        sayHello("Juan");
+    }
+
+
+    // Método sayHello() que recibe un parámetro de tipo String
+    public static void sayHello(String name) {
+        System.out.println("¡Hola, " + name + "! ¡Bienvenido!");
+
+    }
+
+    // Método sayHello() que no recibe parámetros
+    public static void sayHello() {
+        System.out.println("¡Hola! ¡Bienvenido!");
+    }
+}
+
